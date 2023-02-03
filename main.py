aff1d06b4d289974548810e389cf8b5b731c368b
libinleo <70009634+libinleo@users.noreply.github.com>
2023-02-03 16:39:33 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from views.manager.employee import *
-from views.manager.user import *
-from views.admin.project import *
-from views.admin.role import *
-
-if __name__ == "__main__":  
-    app.run(debug=True)
